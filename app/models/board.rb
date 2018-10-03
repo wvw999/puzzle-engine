e30ec279bae0dee3582fe2ea99f7f5447a8e7e6e
Student <example@example.com>
2018-10-03 11:52:02 -0700

    add more pieces to word space locator

--- app/models/board.rb
+++ app/models/board.rb
@@ -4,9 +4,18 @@ class Board
     @valid_spaces = [11, 12, 13, 14, 15, 16, 17, 18, 21, 22, 23, 24, 25, 26, 27, 28, 31, 32, 33, 34, 35, 36, 37, 38, 41, 42, 43, 44, 45, 46, 47, 48, 51, 52, 53, 54, 55, 56, 57, 58, 61, 62, 63, 64, 65, 66, 67, 68, 71, 72, 73, 74, 75, 76, 77, 78, 81, 82, 83, 84, 85, 86, 87, 88]
     @empty_board = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 99, 99, 99, 99, 99, 99, 99, 99, 1, 1, 99, 99, 99, 99, 99, 99, 99, 99, 1, 1, 99, 99, 99, 99, 99, 99, 99, 99, 1, 1, 99, 99, 99, 99, 99, 99, 99, 99, 1, 1, 99, 99, 99, 99, 99, 99, 99, 99, 1, 1, 99, 99, 99, 99, 99, 99, 99, 99, 1, 1, 99, 99, 99, 99, 99, 99, 99, 99, 1, 1, 99, 99, 99, 99, 99, 99, 99, 99, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
     @steps = [-11, -10, -9, -1, 1, 9, 10, 11]
+    @stepsdown = [-21, -20, -19]
+    @stepsup = [19, 20, 21]
   end
 
-  def make_board(wordlist) # clones board and attempts to place each letter on board
+  # clones board and attempts to place each letter on board
+  def make_board(wordlist)
+    @restart_loop = 0
+    @loop_counter = 0
+    loop_manager(wordlist)
+  end
+
+  def loop_manager(wordlist)
     @blank_board = @empty_board.clone
     wordlist.each do |word|
       list_of_ninenines
@@ -15,7 +24,8 @@ class Board
     end
   end
 
-  def list_of_ninenines # builds list of 99 values on board at the beginning of each word placement loop
+  # builds list of 99 values on board at the beginning of each word placement loop
+  def list_of_ninenines
     @list_of_ninenine = []
     @valid_spaces.each do |ninenine|
       if @blank_board[ninenine] == 99
@@ -24,62 +34,49 @@ class Board
     end
   end
 
-  def word_spaces_loop(length_of_word) # at the end of this loop, should have an array with word.length number of board index locations inside
+  # at the end of this loop, should have an array with word.length number of board index locations inside
+  def word_spaces_loop(length_of_word)
     @candidate_word_array = []
     @candidate_word_array.push(@list_of_ninenine.sample)
-    @list_of_ninenine.delete(@candidate_word_array[-1]) # places the first letter of the word in the array so the loop has a -1 to work with
+    # places the first letter of the word in the array so the loop has a -1 to work with
+    @list_of_ninenine.delete(@candidate_word_array[-1])
 
     (length_of_word-1).times do |pickspaces|
       @list_of_neighbors = []
+
+      #builds array of index locations in @blank_board
       @steps.each do |buildneighbors|
-        if @list_of_ninenine.include?(@candidate_word_array[-1] + buildneigbors)
-          @list_of_neighbors.push(@candidate_word_array[-1] + buildneigbors) #builds array of index locations in @blank_board
+        if @list_of_ninenine.include?(@candidate_word_array[-1] + buildneighbors)
+          @list_of_neighbors.push(@candidate_word_array[-1] + buildneighbors)
         end
       end
-      if @list_of_neighbors
+
+      #removes the newly selected next space on board from the array of candidates in list_of_ninenine
+      if @list_of_neighbors.length > 0
         @candidate_word_array.push(@list_of_neighbors.sample)
-        @list_of_ninenine.delete(@candidate_word_array[-1]) #removes the newly selected next space on board from the array of candidates in list_of_ninenine
+        @list_of_ninenine.delete(@candidate_word_array[-1])
+      elsif @list_of_ninenine > 0
+        # if the neighbor check fails, look again for the +10 values using second array of plus/minus values
+
+
       else
+        @restart_loop = 1
+        @loop_counter += 1
         binding.pry
       end
     end
 
   end
 
-  def update_blank_board(word) # takes the candidate word array and adds the letters from the word to the board
+  # takes the candidate word array and adds the letters from the word to the board
+  def update_blank_board(word)
     word_letters = word.split("")
     word_length = word.length
     word_index = 0
     word_length.times do |updateboard|
-      @blank_board[@@candidate_word_array[word_index]] = word_letters[word_index]
-      word_index ++
+      @blank_board[@candidate_word_array[word_index]] = word_letters[word_index]
+      word_index += 1
     end
-    binding_pry
   end
-
-  # pseudocode begins
-  # 
-  # def thingy
-  #
-  #     clone of blank board 'candidate board' X #plain old copy of empty board to begin the board building process
-  #
-  #       'list of 99s' from 'candidate board' X #this is the index of a space with 99 from the candidate board
-  #
-  #       loop init
-  #         declare 'candidate word array'
-  #         select space 1 randomly from 'list of 99s'
-  #         push space 1 to 'candidate word array'
-  #         remove space 1 from 'list of 99s'
-  #         loop begin
-  #           build new array 'list of neighbors' of 'candidate word array'[-1]
-  #           select subsequent space randomly from 'list of neighbors', push to 'candidate word array'
-  #           remove subsequent space from 'list of 99s'
-  #           (do until end of word or no neighbors found)
-  #         end loop
-  #       end loop
-  #
-  #       at the end of each loop, push values from 'candidate word array' to 'candidate board'
-  #
-  # end
 
 end
