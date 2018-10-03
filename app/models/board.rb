1de3854a0fc6906292675fa60c5d467b9a179498
Student <example@example.com>
2018-10-03 10:02:23 -0700

    add new pieces to board builder

--- app/models/board.rb
+++ app/models/board.rb
@@ -7,20 +7,54 @@ class Board
   end
 
   def make_board(wordlist)
-    start_space = valid_spaces.sample
-    # wordlist.each do
-
-    return @current_open
+    @blank_board = @empty_board.clone
+    wordlist.each do |word|
+      list_of_ninenines
+      word_space_loop(word.length, word)
+    end
   end
 
-  def valid_spaces
-    @current_open = []
+  def list_of_ninenines
+    @list_of_ninenine = []
     @valid_spaces.each do |ninenine|
-      if @empty_board[ninenine] == 99
-        @current_open.push(ninenine)
+      if @blank_board[ninenine] == 99
+        @list_of_ninenine.push(ninenine)
       end
     end
-    @current_open
+  end
+
+  def word_space_loop(length_of_word, candidate_word)
+    candidate_word_array = []
+    candidate_word_array.push(@list_of_ninenine)
+    @list_of_ninenine.delete(candidate_word_array[-1])
+    (length_of_word-1).times do |pickspaces|
+
+    end
+  end
+
+  # pseudocode begins
+
+  def thingy
+
+      clone of blank board 'candidate board' X #plain old copy of empty board to begin the board building process
+
+        'list of 99s' from 'candidate board' X #this is the index of a space with 99 from the candidate board
+
+        loop init
+          declare 'candidate word array'
+          select space 1 randomly from 'list of 99s'
+          push space 1 to 'candidate word array'
+          remove space 1 from 'list of 99s'
+          loop begin
+            build new array 'list of neighbors' of 'candidate word array'[-1]
+            select subsequent space randomly from 'list of neighbors', push to 'candidate word array'
+            remove subsequent space from 'list of 99s'
+            (do until end of word or no neighbors found)
+          end loop
+        end loop
+
+        at the end of each loop, push values from 'candidate word array' to 'candidate board'
+
   end
 
 end
