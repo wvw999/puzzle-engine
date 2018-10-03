4d576c483feb430c0c2505acc6c825b909866b16
Student <example@example.com>
2018-10-03 13:03:22 -0700

    add some adjustments to the arrays

--- app/models/board.rb
+++ app/models/board.rb
@@ -4,17 +4,18 @@ class Board
     @valid_spaces = [11, 12, 13, 14, 15, 16, 17, 18, 21, 22, 23, 24, 25, 26, 27, 28, 31, 32, 33, 34, 35, 36, 37, 38, 41, 42, 43, 44, 45, 46, 47, 48, 51, 52, 53, 54, 55, 56, 57, 58, 61, 62, 63, 64, 65, 66, 67, 68, 71, 72, 73, 74, 75, 76, 77, 78, 81, 82, 83, 84, 85, 86, 87, 88]
     @empty_board = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 99, 99, 99, 99, 99, 99, 99, 99, 1, 1, 99, 99, 99, 99, 99, 99, 99, 99, 1, 1, 99, 99, 99, 99, 99, 99, 99, 99, 1, 1, 99, 99, 99, 99, 99, 99, 99, 99, 1, 1, 99, 99, 99, 99, 99, 99, 99, 99, 1, 1, 99, 99, 99, 99, 99, 99, 99, 99, 1, 1, 99, 99, 99, 99, 99, 99, 99, 99, 1, 1, 99, 99, 99, 99, 99, 99, 99, 99, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
     @steps = [-11, -10, -9, -1, 1, 9, 10, 11]
-    @stepsdown = [-21, -20, -19]
-    @stepsup = [19, 20, 21]
+    @stepsone = [-19, -20, -21, -29, -30, -31, -39, -40, -41, -49, -50, -51, -59, -60, -61, -69, -70, -71]
+    @steptwo = [19, 20, 21, 29, 30, 31, 39, 40, 41, 49, 50, 51, 59, 60, 61, 69, 70, 71]
   end
 
-  # clones board and attempts to place each letter on board
+  # begins process
   def make_board(wordlist)
     @restart_loop = 0
     @loop_counter = 0
     loop_manager(wordlist)
   end
 
+  # runs through the steps needed to place one word on the board, loops over all words
   def loop_manager(wordlist)
     @blank_board = @empty_board.clone
     wordlist.each do |word|
@@ -56,7 +57,7 @@ class Board
         @candidate_word_array.push(@list_of_neighbors.sample)
         @list_of_ninenine.delete(@candidate_word_array[-1])
       elsif @list_of_ninenine > 0
-        # if the neighbor check fails, look again for the +10 values using second array of plus/minus values
+        # if the neighbor check fails the array will be empty, look again for the +10 values using second array of plus/minus values
 
 
       else
