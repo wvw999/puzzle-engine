8a6b51161a52a7a785c262901bf49a5437e2a0d1
Will Swanson <wcs97214@gmail.com>
2018-10-17 09:10:18 -0700

    a few more notes in the notes.md

--- app/models/board.rb
+++ app/models/board.rb
@@ -1,18 +1,25 @@
 class Board
 
   def initialize
+    # lots of ugly data required for dealing with the game board
+    # indexes of all '99' values on a starting board. used to check for remaining '99's as board is being built
     @valid_spaces = [11, 12, 13, 14, 15, 16, 17, 18, 21, 22, 23, 24, 25, 26, 27, 28, 31, 32, 33, 34, 35, 36, 37, 38, 41, 42, 43, 44, 45, 46, 47, 48, 51, 52, 53, 54, 55, 56, 57, 58, 61, 62, 63, 64, 65, 66, 67, 68, 71, 72, 73, 74, 75, 76, 77, 78, 81, 82, 83, 84, 85, 86, 87, 88]
     @empty_board = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 99, 99, 99, 99, 99, 99, 99, 99, 1, 1, 99, 99, 99, 99, 99, 99, 99, 99, 1, 1, 99, 99, 99, 99, 99, 99, 99, 99, 1, 1, 99, 99, 99, 99, 99, 99, 99, 99, 1, 1, 99, 99, 99, 99, 99, 99, 99, 99, 1, 1, 99, 99, 99, 99, 99, 99, 99, 99, 1, 1, 99, 99, 99, 99, 99, 99, 99, 99, 1, 1, 99, 99, 99, 99, 99, 99, 99, 99, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+    # values to locate neighboring spaces, values are "current index plus [this value]"
     @steps = [-11, -10, -9, -1, 1, 9, 10, 11]
+    # if no immediate neighbors are found, will try to look up a row further, or down a row further
     @stepsdownone = [-19, -20, -21]
     @stepsupone = [19, 20, 21]
     @stepsdowntwo = [-29, -30, -31]
     @stepsuptwo = [29, 30, 31]
+    # giving up and looking at all remaining far-away but valid spaces
     @stepsdownthree = [-39, -40, -41, -49, -50, -51, -59, -60, -61, -69, -70, -71]
     @stepsupthree = [39, 40, 41, 49, 50, 51, 59, 60, 61, 69, 70, 71]
+    # list of all 'step' spaces, is used by 'good_neighbor' check
+    # which will tell the board-building attempt to fail if there is a blank space on the board with no neighbors
     @stepsall = [-11, -10, -9, -1, 1, 9, 10, 11, -19, -20, -21, 19, 20, 21, -29, -30, -31, 29, 30, 31, -39, -40, -41, -49, -50, -51, -59, -60, -61, -69, -70, -71, 39, 40, 41, 49, 50, 51, 59, 60, 61, 69, 70, 71]
-    @loop_counter = 1
-    @list_of_neighbors = ["q"]
+    @loop_counter = 0
+    @list_of_neighbors = []
     @candidate_word_array = []
     # example of candidate word array : [[[1,"c"], [2, "a"], [3, "t"]], [[4, "d"], [5, "o"], [6, "g"]]]
     # two words, at board index 1,2,3 and 4,5,6 respectively (not actually valid), each word stored as an array of arrays
@@ -37,6 +44,7 @@ class Board
       # look for spaces current word
       total_bastard
       # add current word to solution array
+      word_to_board
     end
 
     def update_disposable_board
@@ -88,6 +96,10 @@ class Board
       @word_length.times do |search|
 
       end
+    end
+
+    def word_to_board
+      # coming soon
     end
 
   end
